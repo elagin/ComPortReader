79e2460d136d583b85b7f3b9993be6c6d755bbb8
Elagin Pavel <elagin.pasha@gmail.com>
2018-04-04 13:49:32 +0300

    Go!

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ComPortReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboPorts = new System.Windows.Forms.ComboBox();
+            this.buttonRescan = new System.Windows.Forms.Button();
+            this.textBoxUpdateTime = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(13, 40);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(94, 40);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(538, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboPorts
+            // 
+            this.comboPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboPorts.FormattingEnabled = true;
+            this.comboPorts.Location = new System.Drawing.Point(13, 13);
+            this.comboPorts.Name = "comboPorts";
+            this.comboPorts.Size = new System.Drawing.Size(121, 21);
+            this.comboPorts.TabIndex = 3;
+            // 
+            // buttonRescan
+            // 
+            this.buttonRescan.Location = new System.Drawing.Point(140, 13);
+            this.buttonRescan.Name = "buttonRescan";
+            this.buttonRescan.Size = new System.Drawing.Size(75, 23);
+            this.buttonRescan.TabIndex = 4;
+            this.buttonRescan.Text = "Обновить";
+            this.buttonRescan.UseVisualStyleBackColor = true;
+            this.buttonRescan.Click += new System.EventHandler(this.buttonRescan_Click);
+            // 
+            // textBoxUpdateTime
+            // 
+            this.textBoxUpdateTime.Location = new System.Drawing.Point(13, 73);
+            this.textBoxUpdateTime.Name = "textBoxUpdateTime";
+            this.textBoxUpdateTime.Size = new System.Drawing.Size(140, 20);
+            this.textBoxUpdateTime.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxUpdateTime);
+            this.Controls.Add(this.buttonRescan);
+            this.Controls.Add(this.comboPorts);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboPorts;
+        private System.Windows.Forms.Button buttonRescan;
+        private System.Windows.Forms.TextBox textBoxUpdateTime;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace ComPortReader
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort;
+        readonly string DATE_FORMAT = "dd.MM.yyyy HH:mm:ss";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosed += new FormClosedEventHandler(this.MainForm_FormClosed);
+            scanPorts();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String comName = comboPorts.SelectedItem.ToString();
+                serialPort = new SerialPort();
+                serialPort.PortName = comName;
+                serialPort.BaudRate = 9600;
+                serialPort.ReadTimeout = 500;
+                serialPort.WriteTimeout = 500;
+                serialPort.Open();
+                serialPort.DataReceived += serialPort2_DataReceived;
+                buttonStart.Enabled = false;
+                buttonStop.Enabled = true;
+                buttonRescan.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                serialPort = null;
+                string caption = "Ошибка.";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+                result = MessageBox.Show(ex.Message, caption, buttons, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            closePort();
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+            buttonRescan.Enabled = true;
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            closePort();
+        }
+
+        private void closePort()
+        {
+            if (serialPort != null)
+            {
+                serialPort.Close();
+                serialPort = null;
+            }
+        }
+
+        private void serialPort2_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string POT = serialPort.ReadLine();
+            BeginInvoke(new LineReceivedEvent(LineReceived), POT);
+        }
+
+        private delegate void LineReceivedEvent(string POT);
+
+        private void LineReceived(string line)
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"WriteLines2.csv", true))
+            {
+                String now = DateTime.Now.ToString(DATE_FORMAT);
+                String res = now + ";" + line;
+                file.Write(res);
+                textBox1.Text = line;
+                textBoxUpdateTime.Text = now;
+            }
+        }
+
+        private void buttonRescan_Click(object sender, EventArgs e)
+        {
+            scanPorts();
+        }
+
+        private void scanPorts()
+        {
+            comboPorts.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            for (int i = 0; i < ports.Length; i++)
+            {
+                comboPorts.Items.Add(ports[i]);
+            }
+            if (ports.Length > 0)
+                comboPorts.SelectedIndex = 0;
+            if (ports.Length == 1 || ports.Length == 0)
+                comboPorts.Enabled = false;
+        }
+    }
+}
